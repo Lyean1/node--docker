4495ff47e74affac5b94d1f90c6ed9523ee7069a
Lyean <lyean1500@gmail.com>
2023-09-01 20:25:45 +0630

    update

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const ronin = require('ronin-server')
-const mocks = require('ronin-mocks')
+const ronin 		= require( 'ronin-server' )
+const database  = require( 'ronin-database' )
+const mocks 		= require( 'ronin-mocks' )
 
-const server = ronin.server()
+async function main() {
 
-server.use('/', mocks.server(server.Router(), false, true))
-server.start()
+    try {
+    await database.connect( process.env.CONNECTIONSTRING )
+    
+    const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+        server.use( '/foo', (req, res) => {
+            return res.json({ "foo": "bar" })
+        })
+        server.use( '/', mocks.server( server.Router()) )
+
+    const result = await server.start()
+        console.info( result )
+    
+    } catch( error ) {
+        console.error( error )
+    }
+}
+
+main()
